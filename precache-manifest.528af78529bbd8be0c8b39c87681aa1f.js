e761f9f63794961cdc7d84230d98772c6f26cab6
sallabh <sallabhk@gmail.com>
2019-09-05 18:39:55 +0000

    Updates

---
+++ precache-manifest.528af78529bbd8be0c8b39c87681aa1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3663cad2fbbe51871159b555a99f3709",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "223c2194325fd916f4be",
+    "url": "/movie-search/static/css/main.5564babd.chunk.css"
+  },
+  {
+    "revision": "e6074ad290ddac660e7d",
+    "url": "/movie-search/static/js/2.38fdab2e.chunk.js"
+  },
+  {
+    "revision": "223c2194325fd916f4be",
+    "url": "/movie-search/static/js/main.60fbb2d2.chunk.js"
+  },
+  {
+    "revision": "a1de4818153458f3b530",
+    "url": "/movie-search/static/js/runtime~main.ea3c558b.js"
+  }
+]);
